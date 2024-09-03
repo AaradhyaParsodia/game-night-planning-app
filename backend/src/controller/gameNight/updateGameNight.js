c6872b112a23727754c3daabd2d837489d201230
AaradhyaParsodia <aaradhyaparsodia@gmail.com>
2024-09-03 23:42:09 +0530

    minor change in update game night endpoint

--- backend/src/controller/gameNight/updateGameNight.js
+++ backend/src/controller/gameNight/updateGameNight.js
@@ -3,6 +3,7 @@ import Users from "../../model/user.js";
 
 export const updateGameNight = async (req, res)=>{
     const { gameCode } = req.params;
+    const body = req.body;
     try {
         const user = await Users.findOne({ username: req.email });
         const gameNightData = await GameNight.findOne({ gameCode: gameCode, isDeleted: false});
@@ -13,7 +14,21 @@ export const updateGameNight = async (req, res)=>{
             });
         }
 
-        
+        await GameNight.updateOne(
+            {gameCode: gameCode, isDeleted: false},
+            {
+                title: body.title ? body.title : gameNightData.title,
+                time: body.time ? new Date(body.time) : gameNightData.time,
+                location: body.location ? body.location : gameNightData.location,
+                maxPlayers: body.maxPlayers ? body.maxPlayers : gameNightData.maxPlayers,
+                isActive: body.isActive ? body.isActive : gameNightData.isActive,
+                gameId: body.gameId ? body.gameId : gameNightData.gameId
+            }
+        );
+
+        res.status(200).json({
+            message: "Game night updated successfully",
+        });
     } catch (error) {
         console.error(`error in updateGameNight controller ${error}`);
         res.status(500).send({ message: "Something went gone :(\nInternal Server Error" });
